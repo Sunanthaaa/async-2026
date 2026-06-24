f8105b7f8bf827ae571b56621aa62024d45967a7
Sunantha <6710301019@cdti.ac.th>
2026-06-24 10:58:24 +0700

    Update Python files

--- Week1/coffee01_synchronous.py
+++ Week1/coffee01_synchronous.py
@@ -4,29 +4,11 @@ import threading
 
 # ฟังก์ชันจำลองการทำกาแฟให้ลูกค้า 1 คนแบบซิงโครนัส
 def make_coffee(customer_name):
-
-    pid = os.getpid()
-    thread_id = threading.current_thread().native_id
-    thread_name = threading.current_thread().name
-
-    print(f"{ctime()} | [PID: {pid}] [ID: {thread_id}] [Thread Name: {thread_name}] กำลังชงกาแฟให้ ลูกค้า {customer_name}...")
-    sleep(5)
-    print(f"{ctime()} | [PID: {pid}] [ID: {thread_id}] [Thread Name: {thread_name}] ลูกค้า {customer_name}: ได้รับกาแฟแล้ว!")
+    pass
 
 def main():
-
-    queue = ['A', 'B', 'C']
-    main_pid = os.getpid()
-    main_tid = threading.current_thread().native_id
-
-    print(f"{ctime()} | [Main PID: {main_pid}] [Main TID: {main_tid}] === เริ่มระบบจำลองร้านกาแฟแบบ Synchronous ===")
-    start_time = time()
-
-    for customer in queue:
-        make_coffee(customer)
-
-    duration = time() - start_time
-    print(f"{ctime()} | ใช้เวลารวมทั้งหมด: {duration:.2f} วินาที")
+    pass
 
 if __name__ == "__main__":
     main()
+    
