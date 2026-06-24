ea7a4e532fad9d8f0aec404753436acbfba49cd7
Sunantha <6710301019@cdti.ac.th>
2026-06-24 10:41:08 +0700

    pid01-synchronous.py

--- Week1/coffee01_synchronous.py
+++ Week1/coffee01_synchronous.py
@@ -1,14 +1,32 @@
 from time import sleep, ctime, time
+import os
+import threading
 
-# ฟังก์ชันจำลองการทำกาแฟให้ลูกค้า 1 คน
+# ฟังก์ชันจำลองการทำกาแฟให้ลูกค้า 1 คนแบบซิงโครนัส
 def make_coffee(customer_name):
-    pass
+
+    pid = os.getpid()
+    thread_id = threading.current_thread().native_id
+    thread_name = threading.current_thread().name
+
+    print(f"{ctime()} | [PID: {pid}] [ID: {thread_id}] [Thread Name: {thread_name}] กำลังชงกาแฟให้ ลูกค้า {customer_name}...")
+    sleep(5)
+    print(f"{ctime()} | [PID: {pid}] [ID: {thread_id}] [Thread Name: {thread_name}] ลูกค้า {customer_name}: ได้รับกาแฟแล้ว!")
 
 def main():
-    # คิวลูกค้า
-    pass
 
-# สั่งให้โปรแกรมทำงาน
+    queue = ['A', 'B', 'C']
+    main_pid = os.getpid()
+    main_tid = threading.current_thread().native_id
+
+    print(f"{ctime()} | [Main PID: {main_pid}] [Main TID: {main_tid}] === เริ่มระบบจำลองร้านกาแฟแบบ Synchronous ===")
+    start_time = time()
+
+    for customer in queue:
+        make_coffee(customer)
+
+    duration = time() - start_time
+    print(f"{ctime()} | ใช้เวลารวมทั้งหมด: {duration:.2f} วินาที")
+
 if __name__ == "__main__":
     main()
-    
